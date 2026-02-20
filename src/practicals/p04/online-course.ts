fe7af9c1b1ccc344d9b47166d024e8a6a204f5f4
TEK <68010401@kmitl.ac.th>
2026-02-20 18:49:38 +0700

    15

--- src/practicals/p04/online-course.ts
+++ src/practicals/p04/online-course.ts
@@ -1,10 +1,37 @@
 export class OnlineCourse {
-    courseName : string = "";
-    MaxStudents : number = 0;
-    private enrolledStudents : number = 0;
-    private isOpen : boolean = false;
+    courseName:string=""
+    maxStudents:number=0
+    private enrolledStudents:number=0
+    isOpen:boolean=true
 
-    constructor(courseName : string, MaxStudents : number){
+    constructor(courseName:string,maxStudents:number,enrolledStudents:number,isOpen:boolean=true){
         this.courseName = courseName;
-        this.MaxStudents = MaxStudents;
+        this.maxStudents = maxStudents;
+        this.enrolledStudents = enrolledStudents;
+        this.isOpen = isOpen;
     }
+
+    enroll():boolean{
+        if(this.isOpen && this.enrolledStudents<this.maxStudents){
+            this.enrolledStudents++
+            console.log(`Successfully enrolled in ${this.courseName}.`)
+            return true
+        }else{
+            console.log(`false  (Course Full)`)
+            return false
+        }
+    }
+    closeCourse(){
+        this.isOpen=false
+        console.log(`${this.courseName} is now closed for enrollment.`)
+    }
+    getAvailableSeats():number{
+        return this.maxStudents-this.enrolledStudents
+    }
+    canEnroll():boolean{
+        return this.isOpen && this.enrolledStudents<this.maxStudents
+    }
+    getCourseStatus():string{
+        return this.isOpen?"Open":"Closed"
+    }
+}
--- src/practicals/p05/credit-card.ts
+++ src/practicals/p05/credit-card.ts
@@ -1,1 +1,18 @@
-export class CreditCard {}
+export class CreditCard {
+    private cardNumber : string = "";
+    constructor(accountName : string, cardNumber : string){
+        this.cardNumber = cardNumber;
+    }
+
+    getCardNumber(){
+        return this.cardNumber;
+    }
+
+    processPayment(amount : number): boolean{
+        return true;
+    }
+
+    getPaymentFee(amount : number): number{
+        return amount * 0.02;
+    }
+}
